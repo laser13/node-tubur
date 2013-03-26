73b461f74fc0208bc7b02184868ad52d5f5719a4
Павленов Семён <pavlenov@realweb.ru>
2013-03-26 17:44:36 +0400

    0.0.34

--- test/test.js
+++ test/test.js
@@ -17,27 +17,49 @@ var tubur = require('./../index'),
 
 global.dump = tubur.utils.dump;
 
-var TestModel = function(initial) {
+function TestModel1(initial) {
+
+    this.nmb = new Field({ type: 'number[]', required: true });
+    this.name = new Field({ type: 'string', min: 5, max: 15, required: true });
+    this.date = new Field({ type: 'Date' });
+    this.xxx = new Field({ type: 'number', min: 5, max: 15, default: 0 });
+
+    TestModel1.super_.call(this, initial);
+
+}
+util.inherits(TestModel1, EasyStructure);
+
+function TestModel2(initial) {
 
     this.nmb = new Field({ type: 'number', required: true });
-    this.name = new Field({ type: 'string', min: 5, max: 15 });
-    this.date = new Field({ type: 'Date' });
-    this.xxx = new Field({ type: 'number', min: 5, max: 15 });
+    this.test1 = new Field({ type: 'TestModel1', instance: TestModel1, required: true });
 
-    TestModel.super_.call(this, initial);
+    TestModel2.super_.call(this, initial);
 
-};
-util.inherits(TestModel, EasyStructure);
+}
+util.inherits(TestModel2, EasyStructure);
 
+function TestModel3(initial) {
 
-var textModel = new TestModel({
+    this.nmb = new Field({ type: 'number', required: true });
+    this.test1 = new Field({ type: 'Array', instance: TestModel1, required: false });
+//    this.test2 = new Field({ type: 'object', instance: TestModel2, required: true });
 
-    nmb: 25,
-    name: 'Dasher',
-    date: new Date
+    TestModel3.super_.call(this, initial);
+
+}
+util.inherits(TestModel3, EasyStructure);
+
+var textModel = new TestModel3({
+
+    nmb: 24,
+    test1: [
+        { nmb: [56] },
+        { nmb: 389 }
+    ]
 
 });
 
-dump(textModel.isValid(), 0);
-dump(textModel.getErrors(), 4);
-dump(textModel.toObject(), 2);
+dump('isValid: ', textModel.isValid(), 0);
+dump('getErrors: ', textModel.getErrors(), 4);
+dump('toObject: ', textModel.toObject(), 2);
