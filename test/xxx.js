21e5b328af673f70e462bd76f5a43f3f8303d48f
Павленов Семён <pavlenov@realweb.ru>
2013-05-07 11:47:21 +0400

    0.0.80

--- test/xxx.js
+++ test/xxx.js
@@ -8,7 +8,7 @@
  */
 
 var tubur = require('./../index'),
-    util = require('util'),
+    $util = require('util'),
 
     fields = tubur.fields,
     EasyStructure = tubur.EasyStructure,
@@ -21,44 +21,57 @@ var tubur = require('./../index'),
 
 global.dump = new tubur.Collector({ colored: true });
 
+function XStruct() {
 
-wait.turn(function(ok) {
+    this.CdnNmb = new fields.Field({ required: true, type: "number", alias: 'cdnNmb' });
+    this.CdnRequestData = new fields.Field({ required: false, type: "string", alias: 'cdnRequestData' });
 
-    var i = 0;
-    var si = setInterval(function() {
+    XStruct.super_.apply(this, arguments);
+}
+$util.inherits(XStruct, tubur.EasyStructure);
 
-        console.log('1-' + i);
-        i++;
+var x = new XStruct({ cdnNmb: 45, cdnRequestData: 'werty' });
 
-        if (i > 7) {
-            clearInterval(si);
-            ok(321);
-        }
+dump.info(x, '++++++++++++++++++', x.toObject());
 
-    }, 1000);
 
-});
-
-wait.turn(function(ok) {
-
-    var i = 0;
-    var si = setInterval(function() {
-
-        console.log('2-' + i);
-        i++;
-
-        if (i > 7) {
-            clearInterval(si);
-            ok(123);
-        }
-
-    }, 500);
-
-});
-
-wait.run(function(err) {
-
-    if (err) dump.error(err);
-    dump.info('end');
-
-});
+//wait.turn(function(ok) {
+//
+//    var i = 0;
+//    var si = setInterval(function() {
+//
+//        console.log('1-' + i);
+//        i++;
+//
+//        if (i > 7) {
+//            clearInterval(si);
+//            ok(321);
+//        }
+//
+//    }, 1000);
+//
+//});
+//
+//wait.turn(function(ok) {
+//
+//    var i = 0;
+//    var si = setInterval(function() {
+//
+//        console.log('2-' + i);
+//        i++;
+//
+//        if (i > 7) {
+//            clearInterval(si);
+//            ok(123);
+//        }
+//
+//    }, 500);
+//
+//});
+//
+//wait.run(function(err) {
+//
+//    if (err) dump.error(err);
+//    dump.info('end');
+//
+//});
