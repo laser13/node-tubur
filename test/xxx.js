4a67d16eed09710e2a03d74647e8bc5bc1047814
Павленов Семён <pavlenov@realweb.ru>
2013-05-08 14:59:35 +0400

    0.0.84

--- test/xxx.js
+++ test/xxx.js
@@ -21,56 +21,65 @@ var tubur = require('./../index'),
 
 global.dump = new tubur.Collector({ colored: true });
 
-//function XStruct() {
+function XStruct() {
+
+    this.CdnNmb = new fields.Field({ required: true, type: "number", alias: 'cdnNmb', choices: [12,56] });
+    this.CdnRequestData = new fields.Field({ required: false, type: "string", alias: 'cdnRequestData' });
+
+    XStruct.super_.apply(this, arguments);
+}
+$util.inherits(XStruct, EasyStructure);
+
+
+var validator = new EasyStructure();
+
+validator
+    .addField('ddd', { required: true, type: "number" })
+    .addField('kkk', { required: true, type: "string" });
+
+dump.info(validator.isValid({ ddd: '45' }), validator.getError(), validator.toObject());
+
+var xStruct = new XStruct({});
+
+dump.info(xStruct.isValid(), xStruct.getError());
+
+
+//wait
+//    .turn(function(ok) {
 //
-//    this.CdnNmb = new fields.Field({ required: true, type: "number", alias: 'cdnNmb', choices: [12,56] });
-//    this.CdnRequestData = new fields.Field({ required: false, type: "string", alias: 'cdnRequestData' });
+//        var i = 0;
+//        var si = setInterval(function() {
 //
-//    XStruct.super_.apply(this, arguments);
-//}
-//$util.inherits(XStruct, tubur.EasyStructure);
+//            console.log('1-' + i);
+//            i++;
 //
-//var x = new XStruct({ cdnNmb: 45, cdnRequestData: 'werty' });
+//            if (i > 3) {
+//                clearInterval(si);
+//                wait.stop(321);
+//            }
 //
-//dump.info(x.getErrors());
-
-
-wait
-    .turn(function(ok) {
-
-        var i = 0;
-        var si = setInterval(function() {
-
-            console.log('1-' + i);
-            i++;
-
-            if (i > 3) {
-                clearInterval(si);
-                wait.stop(321);
-            }
-
-        }, 1000);
-
-    })
-    .turn(function(ok) {
-
-        var i = 0;
-        var si = setInterval(function() {
-
-            console.log('2-' + i);
-            i++;
-
-            if (i > 7) {
-                clearInterval(si);
-                ok(123);
-            }
-
-        }, 500);
-
-    })
-    .run(function(err) {
-
-    if (err) dump.error(err);
-    dump.info('END');
-
-});
+//        }, 1000);
+//
+//    })
+//    .turn(function(ok) {
+//
+//        var i = 0;
+//        var si = setInterval(function() {
+//
+//            console.log('2-' + i);
+//            i++;
+//
+//            if (i > 7) {
+//                clearInterval(si);
+//                ok(123);
+//            }
+//
+//        }, 500);
+//
+//    })
+//    .run(function(err) {
+//
+//    if (err) dump.error(err);
+//    dump.info('END');
+//
+//});
