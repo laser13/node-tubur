7ce9f2c1cb56a9fce0d2c8cc534616f729b714d7
Павленов Семён <pavlenov@realweb.ru>
2013-08-30 08:42:21 +0400

    ===

--- test/xxx.js
+++ test/xxx.js
@@ -11,7 +11,7 @@ var $util = require('util');
 var fields = $tubur.fields;
 var EasyStructure = $tubur.EasyStructure;
 
-var dump = new $tubur.Collector({ colored: true });
+var dump = new $tubur.Collector({ colored: true, output: 'mongo', mongoUrl: 'mongodb://localhost:27030/log' });
 
 var field = new fields.Field('Email', { type: 'mixed', required: true });
 
