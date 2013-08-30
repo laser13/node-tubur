8d6607b2fab8cca94dced6f8192b18e0ed0eb303
Павленов Семён <pavlenov@realweb.ru>
2013-08-30 10:45:16 +0400

    0.1.7

--- test/xxx.js
+++ test/xxx.js
@@ -11,10 +11,10 @@ var $util = require('util');
 var fields = $tubur.fields;
 var EasyStructure = $tubur.EasyStructure;
 
-var dump = new $tubur.Collector({ colored: true, output: 'mongo', mongoUrl: 'mongodb://localhost:27030/log' });
+var dump = new $tubur.Collector({ colored: true, output: 'mongo', mongoUrl: 'mongodb://localhost:27030/dsp-log' });
 
 var field = new fields.Field('Email', { type: 'mixed', required: true });
 
 field.set();
 
-dump.error(field.getError(), field.get());
+dump.error(new $tubur.error.CustomError('MongoError', {}, 'Вот такая ошибка'));
